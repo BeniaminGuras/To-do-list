c1f50e5461a43047051bd4dfdf6998d9ceabdb04
Beniamin Guras <gurasbeniamin@gmail.com>
2022-05-22 00:23:03 +0200

    add warehouse

--- src/components/Columns/Columns.js
+++ src/components/Columns/Columns.js
@@ -1,15 +1,18 @@
 import styles from './Columns.module.scss';
 import Card from '../Card/Card';
 import CardForm from '../CardForm/CardForm';
+import { useSelector } from 'react-redux';
 
 
 const Column = props => {
-  console.log('second try', props.id);
+  
+  const cards = useSelector(state => state.cards.filter(card => card.columnId === props.id));
+
     return(
     <article className={styles.column}>
         <h1 className={styles.title}><span className={styles.icon + ' fa fa-' + props.icon}></span>{props.title}</h1>
         <ul className={styles.cards}>
-          {props.cards.map(card => <Card key={card.id} title={card.title} />)}
+          {cards.map(card => <Card key={card.id} title={card.title} />)}
         </ul>
         <CardForm columnId={props.id} action={props.action} />
     </article>
